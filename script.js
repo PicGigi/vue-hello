6d33e9239019aeed484cbddb0b678e69452e5eb9
PicGigi <junior.1997@live.it>
2022-11-15 12:11:33 +0100

    Hello Vue

---
+++ script.js
@@ -0,0 +1,9 @@
+const app = new Vue(
+    {
+        el: '#root',
+        data:{
+            message: 'Devo recuperare 2 lezioni e 2 esercizi in un solo giorno per colpa della febbre',
+            picture: 'help.jpg'
+        }
+    }
+)
